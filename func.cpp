070ea180ac5d6dfe95fb08ded1d89740d0fc2eff
abchihba0 <113386115+abchihba0@users.noreply.github.com>
2025-02-16 13:23:46 +0300

    modified:   file.txt

--- func.cpp
+++ func.cpp
@@ -1,4 +1,4 @@
-#include <func.h>
+#include "func.h"
 #include <iostream>
 
 using namespace std;
--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include "func.h"
+#include <string>
 
 using namespace std;
 
-int main() {
+int main()
+{
+    string line;
+    ifstream fs("file.txt", std::ios::binary);
+    if (fs.is_open())
+    {
+        cout << "ok" << endl;
+    }
+    if (fs.is_open())
+    {
+        while (getline(fs, line))
+        {
+            cout << line << endl;
+        }
+    }
+    
+    fs.close();
 
     return 0;
 }
