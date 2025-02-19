564d5a90875e69bbc4df1725de40e6758427c7ee
abchihba0 <113386115+abchihba0@users.noreply.github.com>
2025-02-19 23:49:19 +0300

    modified:   func.h

--- func.h
+++ func.h
@@ -1,23 +1,28 @@
 #include <list>
 #pragma once
+#include <list>
+#include <string>
+#include <unordered_map>
+
 #define CODE_SIZE 8
 
 
 class Node {
+public:
+    unsigned char symb;       // Символ
+    bool isSymb;             // Указывает, является ли узел символом
+    unsigned int freq;       // Частота символа
     
-    public:
-        unsigned char symb;
-	    unsigned char isSymb;
-	    unsigned int freq;
-        Node();
-        Node(unsigned char symbol, unsigned int count);
-        Node(Node *leftChild, Node *rightChild);
-        void print()const;
-    private:
-        Node *left;
-        Node *right;
+    Node();
+    Node(unsigned char symbol, unsigned int count);
+    Node(Node *leftChild, Node *rightChild);
+    void print() const;
+    Node *left;              // Левый дочерний узел
+    Node *right;             // Правый дочерний узел
 };
 
 bool sortTree(const Node *first, const Node *second);
 
-void makeTree(std::list<Node*>&myTree);
+void makeTree(std::list<Node*>& myTree);
+
+void encode(Node* root, std::string str, std::unordered_map<unsigned char, std::string>& huffmanCode);
